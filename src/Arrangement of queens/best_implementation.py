52efd5cb658a83000aa45c6e04ad43b5171ae5ba
Stepan Tramov <tramovs@list.ru>
2025-10-11 05:19:52 +0300

    Fixed according to PEP8

--- src/Arrangement of queens/best_implementation.py
+++ src/Arrangement of queens/best_implementation.py
@@ -1,40 +1,39 @@
 def is_safe(chessboard, row, col, n):
-
     for i in range(row):
         if chessboard[i][col] == 1:
             return False
-    
+
     i, j = row - 1, col - 1
     while i >= 0 and j >= 0:
         if chessboard[i][j] == 1:
             return False
         i -= 1
         j -= 1
-    
+
     i, j = row - 1, col + 1
     while i >= 0 and j < n:
         if chessboard[i][j] == 1:
             return False
         i -= 1
         j += 1
-    
+
     return True
 
-def foo(chessboard, n, rows, sol):
+
+def placement_queens(chessboard, n, rows=0, sol=0):
     if rows == n:
         return sol + 1
-    
+
     for i in range(n):
         if is_safe(chessboard, rows, i, n):
             chessboard[rows][i] = 1
-            sol = foo(chessboard, n, rows + 1, sol)
+            sol = placement_queens(chessboard, n, rows + 1, sol)
             chessboard[rows][i] = 0
-    
+
     return sol
-                
-    
+
+
 n = int(input())
 chessboard = [[0 for i in range(n)] for j in range(n)]
-solution = foo(chessboard, n, 0, 0)
+solution = placement_queens(chessboard, n)
 print(solution)
-
--- src/Arrangement of queens/iteration_solution.py
+++ src/Arrangement of queens/iteration_solution.py
@@ -1,16 +1,17 @@
 import itertools
+
 n = int(input())
 
-k = 0
+counter = 0
 for permutations in itertools.permutations(range(n)):
     flag = 1
     for i in range(n):
-        for j in range(i+1,n):
-            if abs(permutations[i]-permutations[j]) == abs(i - j):
+        for j in range(i + 1, n):
+            if abs(permutations[i] - permutations[j]) == abs(i - j):
                 flag = 0
                 break
         if flag == 0:
             break
     if flag == 1:
-        k += 1
-print(k)
+        counter += 1
+print(counter)
