13b661b8edb5101e67c3051f3f98f23651c4c1f4
Stepan Tramov <tramovs@list.ru>
2025-10-12 20:07:15 +0300

    Fix code style

--- src/Sort Array/sort algoritm for array.py
+++ src/Sort Array/sort algoritm for array.py
@@ -1,10 +1,10 @@
 a = []
 string = input('Enter the numbers to sort by, separated by spaces: ')
 
-#Adding the entered numbers to the array
+# Adding the entered numbers to the array
 flag = 1
 tmp = ''
-for i in range(0,len(string)):
+for i in range(0, len(string)):
     if string[i] == ' ':
         flag = 0
     elif flag == 0:
@@ -12,17 +12,15 @@ for i in range(0,len(string)):
         tmp = string[i]
     else:
         tmp += string[i]
-        if i+1 == len(string) or string[i+1] == ' ':
+        if i + 1 == len(string) or string[i + 1] == ' ':
             a.append(int(tmp))
 
-#Sorting an array
-for i in range(1,len(a)):
-    while i != 0 and a[i-1] > a[i]:
+# Sorting an array
+for i in range(1, len(a)):
+    while i != 0 and a[i - 1] > a[i]:
         x = a[i]
-        a[i] = a[i-1]
-        a[i-1] = x
+        a[i] = a[i - 1]
+        a[i - 1] = x
         i -= 1
 
 print(a)
-    
-        
