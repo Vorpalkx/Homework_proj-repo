141aca8877e6452053e7ff488970317467c41c34
Stepan Tramov <tramovs@list.ru>
2025-10-15 00:45:49 +0300

    Added stack implementation

---
+++ include/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+
+typedef struct Elem {
+    int value;
+    struct Elem* next;
+} Elem;
+
+typedef struct {
+    Elem* top;
+    int size;
+} Stack;
+
+void newStack(Stack* stack);
+bool isEmpty(Stack* stack);
+void push(Stack* stack, int item);
+int pop(Stack* stack);
+int peek(Stack* stack);
+void deleteStack(Stack* stack);
+
+#endif
---
+++ src/Stack/stack.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
+
+typedef struct Elem {
+    int value;
+    struct Elem* next;
+} Elem;
+
+typedef struct {
+    Elem* top;
+    int size;
+} Stack;
+
+void newStack(Stack* stack)
+{
+    stack->top = NULL;
+    stack->size = 0;
+}
+
+bool isEmpty(Stack* stack)
+{
+    return stack->top == NULL;
+}
+
+void push(Stack* stack, int item)
+{
+    Elem* newElem = malloc(sizeof(Elem));
+    if (!newElem) {
+        printf("Memory allocation error\n");
+        return;
+    }
+    newElem->value = item;
+    newElem->next = stack->top;
+    stack->top = newElem;
+    stack->size++;
+}
+
+int pop(Stack* stack)
+{
+    if (isEmpty(stack)) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+
+    Elem* pointer = stack->top;
+    int item = pointer->value;
+    stack->top = pointer->next;
+    free(pointer);
+    stack->size--;
+
+    return item;
+}
+
+int peek(Stack* stack)
+{
+    if (isEmpty(stack)) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+
+    return stack->top->value;
+}
+
+void deleteStack(Stack* stack)
+{
+    while (!isEmpty(stack)) {
+        pop(stack);
+    }
+
+    free(stack);
+    printf("Stack deleted\n");
+}
