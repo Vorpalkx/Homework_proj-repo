18bac65a841de9522b83a7412e505538823f3475
Stepan Tramov <tramovs@list.ru>
2025-10-16 03:48:02 +0300

    Add a stack-based bracket balance

--- src/Advanced bracket balance/bracket_balance.c
+++ src/Advanced bracket balance/bracket_balance.c
@@ -9,21 +9,18 @@ void bracket_balance(Stack* stack, int* string)
     for (int i = 1; i < counter; i++) {
         for (int j = 0; j < 6; j++) {
             if (string[i] == bracket[j]) {
-                // Checking that the bracket is opening
                 if (j % 2 == 0) {
                     push(stack, string[i]);
                 } else {
                     if (is_empty(stack)) {
-                        printf("The balance of the brackets is not respected\n");
+                        printf(":(\n");
                         return;
                     }
-
-                    // Checking the matching of brackets
                     char top_el = peek(stack);
-                    if (top_el == bracket[j - 1]) {
+                    if (top_el == bracket[j-1]) {
                         pop(stack);
                     } else {
-                        printf("The balance of the brackets is not respected\n");
+                        printf(":(\n");
                         return;
                     }
                 }
@@ -31,12 +28,10 @@ void bracket_balance(Stack* stack, int* string)
             }
         }
     }
-
-    // Checking that there are no brackets left in the stack
     if (is_empty(stack)) {
         printf("The balance of the brackets is respected\n");
     } else {
-        printf("The balance of the brackets is not respected\n");
+        printf(":(\n");
     }
     return;
 }
