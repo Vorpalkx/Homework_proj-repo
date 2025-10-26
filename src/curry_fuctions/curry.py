82d05ad71e5fa00c45df5748c668e891de618129
Stepan Tramov <tramovs@list.ru>
2025-10-26 19:32:43 +0300

    Code-style edits have been made in curry.py

--- src/curry_fuctions/curry.py
+++ src/curry_fuctions/curry.py
@@ -1,29 +1,37 @@
 def curry(func, arity):
     if not isinstance(arity, int):
-        raise TypeError(f"Expected arity type: int, received {type(arity).__name__}")
+        raise TypeError(
+            f"Expected arity type: int, received {type(arity).__name__}"
+        )
 
     if arity < 0:
         raise ValueError(f"Expected arity >= 0, received {arity}")
 
     def collect_args(collected_args):
         def wrapper(next_arg):
-            if len(collected_args) + 1 > arity:
-                raise TypeError(f"""Too many arguments. Expected {arity},
-                    got {len(collected_args) + 1}""")
-                
+            length_args = len(collected_args) + 1
+            if length_args > arity:
+                raise TypeError(
+                    f"Too many arguments. Expected {arity}, got {length_args}"
+                )
+
             new_args = collected_args + (next_arg,)
-            
+
             if len(new_args) == arity:
                 return func(*new_args)
             else:
                 return collect_args(new_args)
+
         return wrapper
-    
+
     return collect_args(())
+
 
 def uncurry(func, arity):
     if not isinstance(arity, int):
-        raise TypeError(f"Expected arity type: int, received {type(arity).__name__}")
+        raise TypeError(
+            f"Expected arity type: int, received {type(arity).__name__}"
+        )
 
     if arity < 0:
         raise ValueError(f"Expected arity >= 0, received {arity}")
@@ -36,5 +44,5 @@ def uncurry(func, arity):
         for arg in args:
             result = result(arg)
         return result
-    return wrapper
 
+    return wrapper
