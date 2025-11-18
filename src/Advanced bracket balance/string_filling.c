1ff0dd63195f67fc78fa9e884746f5b31bd28ef4
Stepan Tramov <tramovs@list.ru>
2025-11-19 02:45:03 +0300

    Formatted files with .c and .h according to the clang-format

--- src/Advanced bracket balance/string_filling.c
+++ src/Advanced bracket balance/string_filling.c
@@ -1,23 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int *string_filling()
+int* string_filling()
 {
     printf("Enter a string of no more than 100 characters:\n");
     char string[100];
     int counter = 1;
     // Entering a string from the keyboard
     while ((counter < 100) && scanf("%c", &string[counter]) == 1) {
-    	    if (string[counter] == '\n')
-	        break;
-	    counter++;
+        if (string[counter] == '\n')
+            break;
+        counter++;
     }
 
     string[0] = counter;
     // Allocating dynamic memory for less memory usage
-    int *array = (int*)malloc(counter*sizeof(int));
+    int* array = (int*)malloc(counter * sizeof(int));
     for (int i = 0; i < counter; i++)
         array[i] = string[i];
-        
+
     return array;
 }
