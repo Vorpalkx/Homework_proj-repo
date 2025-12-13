c89f8627ed4361235c885b77200c30033e4b2ab8
Stepan Tramov <tramovs@list.ru>
2025-12-13 17:49:57 +0300

    ReControlWork

---
+++ src/controlWork/without_pair.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_SIZE 1000
+
+int main()
+{
+    int counter = 0; 
+    int numbers[MAX_SIZE];
+    int input;
+
+    while (counter < MAX_SIZE) {
+        scanf("%d", &input);
+        if (input == 0) {
+            break;
+        }
+        numbers[counter] = input;
+        counter++;
+    }
+
+    for (int i = 0; i < counter; i++) {
+        int cnt = 0;
+        for (int j = 0; j<counter; j++) {
+            if (numbers[i] == numbers[j] - 1) {
+                cnt++;
+            }
+        }
+        if (cnt == 0) {
+            printf("%d ", numbers[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
